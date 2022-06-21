791b2feec3840d0a80131b4e10a465ed05c9a528
ahodori <samuelbhendricks@gmail.com>
2022-06-21 14:40:41 -0400

    submissions routes created, validate & authorize

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -9,4 +9,15 @@ class Submission < ApplicationRecord
     validates :url, presence: true
 
     validates :user_id, uniqueness: { scope: :battle_id, message: "Only one submission per user per battle is allowed"}
+    #validate :battle_id, :battle_exists_and_is_open
+
+    def battle_exists_and_is_open
+        battle = Battle.find_by(battle_id)
+        unless battle
+            errors.add(:battle_id, ", battle does not exist")
+        end
+        unless battle.is_ended == false
+            errors.add(:battle_id, ", battle is already closed to submissions")
+        end
+    end
 end
