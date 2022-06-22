44d37989a1294e659473cc5f6998ca9ab316c445
ahodori <samuelbhendricks@gmail.com>
2022-06-22 13:04:09 -0400

    added show logic to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find_by(id: params[:id])
+        if user
+            render json: user
+        else
+            render json: { error: "User not found" }, status: 404
+        end
+    end
+
     def create
         user = User.create(user_params)
         if user.valid?
